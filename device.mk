6883bb2fa27f6fe55cbe812eb4b7cffb763c2cfa
ImSolkin <stepanportyankin@gmail.com>
2026-07-02 02:22:15 +0300

    CK7n: Remove unused AB OTA post-install configurations and related packages

--- device.mk
+++ device.mk
@@ -26,24 +26,11 @@ AB_OTA_PARTITIONS := \
     vendor_boot \
     vendor_dlkm
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
-    POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
-    update_verifier \
-    otapreopt_script \
-    checkpoint_gc
+    update_verifier
 
 PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
