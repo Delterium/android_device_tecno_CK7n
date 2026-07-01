1ceea50b20ee7b9605e80ec6bbedb7762bf2f4ae
ImSolkin <stepanportyankin@gmail.com>
2026-07-01 15:20:03 +0300

    Update vendorsetup.sh to improve patch application checks and add visibility patch for native_app_zygote

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,29 +3,44 @@
 RET1=0
 RET2=0
 
-echo "- Applying gore transsion fod hack"
 cd frameworks/base
-curl -sL https://github.com/fjrXTR/android_frameworks_base-AXION/commit/cd9e48a500ab62df97ed0d58df8afa848bd0c189.patch | git am || {
-  RET1=$?
-  git am --abort >/dev/null 2>&1
-}
+if git log -50 --oneline | grep -q "Add HBM Trigger for Transsion UDFPS"; then
+  echo "- frameworks/base is already patched, skipping."
+else
+  echo "- Applying frameworks/base fod patch..."
+  curl -sL https://github.com/fjrXTR/android_frameworks_base-AXION/commit/cd9e48a500ab62df97ed0d58df8afa848bd0c189.patch | git am || {
+    RET1=$?
+    git am --abort >/dev/null 2>&1
+  }
+fi
 cd ../../
 
-echo "- Applying system/sepolicy native_app_zygote visibility patch"
 cd system/sepolicy
-curl -sL https://raw.githubusercontent.com/Delterium/patches/seventeen/sepoilicy/sepolicy_patch.patch | git apply || {
-  RET2=$?
-}
+if [ -f "public/native_app_zygote.te" ]; then
+  echo "- system/sepolicy is already patched, skipping."
+else
+  echo "- Applying system/sepolicy native_app_zygote visibility patch..."
+  
+  sed -i 's/^type native_app_zygote, domain;/#type native_app_zygote, domain;/g' private/native_app_zygote.te
+  
+  echo "type native_app_zygote, domain;" > public/native_app_zygote.te
+  
+  if [ -f "public/native_app_zygote.te" ]; then
+    echo "OK: system/sepolicy patched locally."
+  else
+    RET2=1
+  fi
+fi
 cd ../../
 
 if [ $RET1 -ne 0 ]; then
-  echo "ERROR: frameworks/base patch is not applied! Maybe it's already patched, or you'll have to adapt it to this specific rom source?"
+  echo "ERROR: frameworks/base patch is not applied! Maybe you need to run 'git -C frameworks/base am --abort'?"
 else
-  echo "OK: frameworks/base patched successfully"
+  echo "OK: frameworks/base check passed"
 fi
 
 if [ $RET2 -ne 0 ]; then
-  echo "ERROR: system/sepolicy patch is not applied! Maybe it's already patched, or you'll have to adapt it to this specific rom source?"
+  echo "ERROR: system/sepolicy patch is not applied! Please check permissions."
 else
-  echo "OK: system/sepolicy patched successfully"
+  echo "OK: system/sepolicy check passed"
 fi
